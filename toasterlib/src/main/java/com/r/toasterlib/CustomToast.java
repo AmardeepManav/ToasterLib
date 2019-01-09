46c5eb2699022e9808dae927f643d22a833af582
amardeep <amardeep@adoreinfotech.com>
2019-01-09 16:06:56 +0530

    Add one method in Custom Toast

--- toasterlib/src/main/java/com/r/toasterlib/CustomToast.java
+++ toasterlib/src/main/java/com/r/toasterlib/CustomToast.java
@@ -8,4 +8,8 @@ public class CustomToast {
     public static void toaster(Context c, String message){
         Toast.makeText(c, message,Toast.LENGTH_SHORT).show();
     }
+
+    public static void randomToast(Context context , String message) {
+        Toast.makeText(context, "hello..." + message,Toast.LENGTH_SHORT).show();
+    }
 }
